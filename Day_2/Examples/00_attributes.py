cc232b87678ddfccdd2ff19783b24e98dc850e8e
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-01-27 12:50:29 +0300

    Add practice task1

--- Day_2/Examples/00_attributes.py
+++ Day_2/Examples/00_attributes.py
@@ -7,9 +7,9 @@ class People:
     name = "Teachers"
 
 
-# Встроенный(системный)
+# Встроенный(системный) для классов
 # print(People.__name__)
-# People.__name__ = 'Some people'
+# People.__name__ = 'Person'
 # print(People.__name__)
 
 # Сами создаем в классе
@@ -21,6 +21,7 @@ class People:
 # person1 = P1()     # создаю экземпляр на основе класса People
 # print(p is person1)
 # print(type(p), type(person1))
+# print(type(p) is type(person1))
 # print(id(P1))
 # print(id(People) == id(P1))
 # print(People is P1)
@@ -35,37 +36,37 @@ class People:
 # doubt_p = P1()  # Вспомнить и проверить
 # print(doubt_p)
 # print(P1.__name__)
-# p2 = People()  # Out: NameError: name 'People' is not defined
+# # p2 = People()  # Out: NameError: name 'People' is not defined
 # del P1
 # print(p.name)   # Вспомнить и проверить
 #
-#
-# # Получаем словарь объекта
+# # # Получаем словарь объекта
 # print(vars(p))
 # # Создаем снова имя(ссылку) People
+# print(type(p))
 # People = type(p)
 #
 # print(id(People))
 # p9 = People()
 # print(type(p9))
 # pprint(vars(People))
-# # ======================================
-#
-# # class People, создаю новый экземпляр
+# ======================================
+
+# class People, создаю новый экземпляр
 # print(type(p))
 # print(id(p))
 # new_p = type(p)()  # new_p = People()
 # print(type(new_p))
 # print(new_p)
 # print(id(new_p))
-#
-# # # ## Получаю доступ к классу экземпляра
+
+# ## Получаю доступ к классу экземпляра
 # print(p.__class__)  # type(p)
 # print(new_p.__class__)  # type(new_p)
 # print(id(p.__class__))
 # print('=' * 40)
-#
-# # # ## Создаем экземпляр
+
+# # ## Создаем экземпляр
 # p2 = p.__class__()  # p2 = People()
 # print(type(p2), p2)
 # print('=' * 40)
@@ -87,10 +88,10 @@ class People:
 # print(p.__class__.__name__)
 # print(People.__name__)
 #
-# # Но здесь ошибка!
+# # # Но здесь ошибка! У экземпляров атрибута __name__ нет!
 # # print(p.__name__)  # AttributeError
-# print(type(People))  # <class 'type'>
-# print(People.__class__)
+# # print(type(People))  # <class 'type'>
+# # print(People.__class__)
 # p4 = People()
 # print(p4.__class__.__name__)
 
@@ -105,7 +106,7 @@ class People:
 # print(p.name)
 # pprint(People.__dict__)
 # pprint(p.__dict__)  # Out: {}
-#
+
 # # Создаем значение для атрибута экземпляра
 # p.count = 30
 # pprint(p.__dict__)
@@ -115,11 +116,12 @@ class People:
 # print(vars(p).get('name'))  # None
 # print(vars(p).get('count'))  # 30
 #
-# # Проверка наличия атрибута у экземпляра
+# # # Проверка наличия атрибута у экземпляра
 # print(hasattr(p, 'name'))  # p.name
 #
 # del People.name
 # pprint(People.__dict__)
+# print(hasattr(p, 'name'))
 # # print(p.name)  # AttributeError
 # del People.count
 # pprint(People.__dict__)
@@ -129,27 +131,27 @@ class People:
 # p.age = 22
 # pprint(p.__dict__)
 #
-# # функция vars покажет словарь атрибутов объекта
+# # # функция vars покажет словарь атрибутов объекта
 # print(vars(p))
 # pprint(vars(People))
 
 # Меняем значение атрибута класса через экземпляр
-p = People()
+# p = People()
 # p.__class__.name = 'SName'  # People.name = 'SName'
 # pprint(People.__dict__)
 # print(p.__dict__)
-
-# ==========================================
-# Функции getattr, setattr, delattr, hasattr
-# ==========================================
+#
+# # ==========================================
+# # Функции getattr, setattr, delattr, hasattr
+# # ==========================================
 # value = 'name'
-# # # Шаблон вызова: имя объекта, имя атрибута
+# # Шаблон вызова: имя объекта, имя атрибута
 # print(getattr(People, value))  # Out: People.name -> SName
 #
 # # Вернет третий аргумент, если атрибута нет
 # print(getattr(People, 'name2', 'Такого нет'))
-# # # print(People.name2)   # AttributeError
-# #
+# # print(People.name2)   # AttributeError
+#
 # # Шаблон вызова: имя объекта, имя атрибута, значение атрибута
 # field_name = 'course'
 # setattr(People, field_name, 'Python')  # People.course = 'Python'
@@ -159,8 +161,8 @@ p = People()
 # setattr(p, field_name, 'Golang')
 # print("after:")
 # print(p.__dict__)
-
-# # Пример использования функции setattr
+#
+# # # Пример использования функции setattr
 # from string import ascii_letters as chars
 # from random import choice
 # # choice вернет случайный элемент из последовательности
@@ -173,24 +175,25 @@ p = People()
 # # ## Удаляем атрибут
 # # ## Шаблон вызова: имя объекта, имя атрибута
 # delattr(People, 'course')
+# delattr(People, new_field)
 # pprint(People.__dict__)
-
-
+#
+#
 # p.age = 22
 # pprint(vars(p))
 # # # проверка наличия атрибута у объекта(класс, экземпляр)
 # print(hasattr(People, 'age'), hasattr(People, 'name'))
 # print(hasattr(p, "name"), hasattr(p, 'age'))
 # print(p.name)
-#
-#
+
+
 # # Проверка понимания
 # People.some = 567
 # # Это не только проверка словаря экземпляра, но и класса
 # print(hasattr(p, 'some'))  # Вспомнить и проверить
 # print(p.some)
 # print(vars(p).get("some"))
-# print(getattr(p, 'some'))  #
+# print(getattr(p, 'some'))  # 567
 
 # =============
 # Методы класса
@@ -201,6 +204,7 @@ p = People()
 #         self.surname = 'Ivanov'
 #
 #     def hello1(self) -> None:   # list.append(), list.insert(), list.remove(), list.sort(), dict.update()
+#         """ Метод, который меняет состояние объекта"""
 #         self.name += ' Ivanovich'
 #
 #     def hello3(self) -> None:
@@ -229,9 +233,10 @@ p = People()
 # Student.hello3(sb)  # sb.hello3()
 #
 # # # Важный момент с hello2()
-# Student.hello2()      # Отработает
-# # sb.hello2()         # TypeError
-# # Student.hello2(sb)  # TypeError
+# Student.hello2()       # Отработает
+# sb.__class__.hello2()  # и это тоже
+# # sb.hello2()          # TypeError
+# # Student.hello2(sb)   # TypeError
 #
 # print(sb.__dict__)
 # print(sb.hello1.__self__)
