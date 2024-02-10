e39abc156c4fc659873b744abc931ba194bbf403
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-02-10 16:39:00 +0300

    Add 04_decorator

--- Day_4/Examples/03_properties.py
+++ Day_4/Examples/03_properties.py
@@ -1,7 +1,7 @@
 class StudentA:
     """ Test class for learning """
     def __init__(self, name):
-        self._name = name
+        self._name = name  # Приватный атрибут(для внутреннего использования)
 
     def get_name(self):
         print('From get_name()')
@@ -12,6 +12,7 @@ class StudentA:
         self._name = value
 
     def del_name(self):
+        print('From del_name()')
         self._name = ''
 
     # V1
@@ -34,20 +35,19 @@ class StudentA:
     # name = name.deleter(del_name)
 
 
-
-# student = StudentA('first')
+# student = StudentA('Stepan')
 # print(student.__dict__)
 # print(student.name)        # fget=get_name
 #
 # # присвоить новое значение
-# student.name = 'second'  # fset=set_name
+# student.name = 'Nikolay'  # fset=set_name
 # print(student.name)
 # print(student.__dict__)
 #
 # del student.name        # fdel=del_name
 # print(student.__dict__)
 # print(repr(student.name))
-# student.name = 'third'
+# student.name = 'Ivan'
 # print(student.__dict__)
 # print(student.name)
 # print(student._name)
@@ -55,45 +55,45 @@ class StudentA:
 # print('*' * 40)
 
 
-# # Обычно property используют как декоратор
-# class StudentB:
-#     """ Test class for learning """
-#     def __init__(self, name):
-#         self._name = name
-#
-#     @property   # student.name(реализован getter)  name = property(name)
-#     def name(self):
-#         return self._name
-#
-#     # student.name = 12(реализован setter)
-#     # мы должны явно указать имя аттрибута, для которого указываем setter: name = name.setter(name)
-#     @name.setter
-#     def name(self, value):
-#         if len(value) > 3:
-#             self._name = value
-#         else:
-#             raise Exception('Too short')
-#
-#     # del student.name (реализован deleter)
-#     # мы должны явно указать имя аттрибута, для которого указываем deleter
-#     @name.deleter
-#     def name(self):
-#         self._name = ''
-#
-#
-# s_b = StudentB('Ivan')
-# print(s_b.__dict__)
-#
-# print(s_b.name, type(s_b.name))  # Результат работы getter'а
-# # print(s_b.name())   # TypeError: 'str' object is not callable
-#
-# s_b.name = 'Stepan'  # Результат работы setter'а
-# print(s_b.__dict__)
-# print(s_b.name)
-#
-# del s_b.name     # Результат работы deleter'а
-# print(s_b.__dict__)
-# print(f'{s_b.name!r}')  # repr
-#
-# # В setter есть проверка данных
-# s_b.name = 'Ян'  # error Exception: Too small
+# Обычно property используют как декоратор
+class StudentB:
+    """ Test class for learning """
+    def __init__(self, name):
+        self._name = name
+
+    @property   # student.name(реализован getter)  name = property(name)
+    def name(self):
+        return self._name
+
+    # student.name = 12(реализован setter)
+    # мы должны явно указать имя аттрибута, для которого указываем setter: name = name.setter(name)
+    @name.setter
+    def name(self, value):
+        if len(value) > 3:
+            self._name = value
+        else:
+            raise Exception('Too short')
+
+    # del student.name (реализован deleter)
+    # мы должны явно указать имя аттрибута, для которого указываем deleter
+    @name.deleter
+    def name(self):
+        self._name = ''
+
+
+s_b = StudentB('Ivan')
+print(s_b.__dict__)
+
+print(s_b.name, type(s_b.name))  # Результат работы getter'а
+# print(s_b.name())   # TypeError: 'str' object is not callable
+
+s_b.name = 'Stepan'  # Результат работы setter'а
+print(s_b.__dict__)
+print(s_b.name)
+
+del s_b.name     # Результат работы deleter'а
+print(s_b.__dict__)
+print(f'{s_b.name!r}')  # repr
+
+# В setter есть проверка данных
+s_b.name = 'Ян'  # error Exception: Too small
