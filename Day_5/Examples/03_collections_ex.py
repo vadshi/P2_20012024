d20e90b95e8fc43729fe2f237b55d848b93c2417
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-02-17 16:52:19 +0300

    Add sorting and graph

--- Day_5/Examples/03_collections_ex.py
+++ Day_5/Examples/03_collections_ex.py
@@ -3,22 +3,22 @@ from collections import Counter, deque
 
 from pprint import pprint
 
-# Обычный словарь(dict)
+# # Обычный словарь(dict)
 # d_usual = dict()
 # print(type(d_usual))
 # for word in ['spam', 'egg', 'egg', 'spam',
 #              'counter', 'counter', 'counter']:
 #     # Вернет значение ключа, если есть. Создать пару, если такого ключа нет.
 #     d_usual.setdefault(word, 0)
-#     d_usual[word] += 1
+#     d_usual[word] += 1  # d_usual[word] = d_usual[word] + 1
 #
 # print(d_usual.get("col", 0))
 # # print(d_usual['col'])   # Error: KeyError
 # print(d_usual)
-
-# =============
-# класс Counter
-# =============
+#
+# # =============
+# # класс Counter
+# # =============
 # counter = Counter()
 # print(type(counter))
 # for word in ['spam', 'egg', 'egg', 'spam',
@@ -29,7 +29,7 @@ from pprint import pprint
 # print(counter['counter'])
 # print(counter['collections'])  # Нет ошибки
 
-# # Пример создания на лету.
+# Пример создания на лету.
 # print(Counter('count_symbols_in_string'))
 #
 # # Вернуть 3 самых часто встречающихся элемента
@@ -40,11 +40,11 @@ from pprint import pprint
 # # Вернуть 2 самых редко встречающихся элемента
 # print(Counter('hello students, language python').most_common()[:-3:-1])
 # print(Counter('hello students, language python').most_common()[-2:])
-
+#
 # counter = Counter(a=4, b=2, c=0, d=-2)
 # counter2 = Counter(a=1, b=2, c=3, d=4)
-
-# Ключи только с положительными значениями
+#
+# # Ключи только с положительными значениями
 # print('Сложение:\n', counter + counter2)
 # print('Вычитание:\n', counter - counter2)  # Counter({'a': 3})
 #
@@ -101,25 +101,25 @@ py -c "import this" | out-file zen.txt -encoding utf8
 # =================================
 # Deque FIFO -> First In, First Out
 # =================================
-
+#
 # ex = list("python")
 # deque_ex = deque(ex)
 # print(deque_ex)
 # print(deque_ex.__class__.__mro__)
-
+#
 # deque_ex.append('e')      # добавление в конец
 # deque_ex.appendleft('a')  # добавление в начало (левый конец)
 # print(deque_ex)
-
+#
 # right_elem = deque_ex.pop()
 # left_elem = deque_ex.popleft()
 # print(deque_ex)
 # print(f'{right_elem =}, {left_elem = }')
-
+#
 # deque_ex.extend([10, 11])
 # deque_ex.extendleft([98, 99])
 # print(deque_ex)
-
+#
 # print(deque_ex.count('p'))
 # print(deque_ex[3])
 # print(len(deque_ex))
@@ -127,20 +127,21 @@ py -c "import this" | out-file zen.txt -encoding utf8
 # Перенос n элементов из конца очереди в начало
 # deque_ex.rotate(5)
 # print(deque_ex)
-#
+# #
 # Перенос n элементов из начала очереди в конец
 # deque_ex.rotate(-5)
 # print(deque_ex)
 # lst = list(deque_ex)
 # print(lst, type(lst))
 
+
 # Параметр maxlen определяет количество элементов в очереди.
 # def tail(filename, n=10):
 #     """ Возвращает n последних строк файла """
 #     with open(filename) as f:
 #         return deque(f, maxlen=n)
-
-
+#
+#
 # print(*tail('zen.txt'), sep='')
 
 
